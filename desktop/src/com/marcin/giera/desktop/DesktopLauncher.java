029ac9aa2b01750f8f624a1e9dff9fd0955eba17
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2020-11-14 12:36:51 +0100

    Start of the Project!

---
+++ desktop/src/com/marcin/giera/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.marcin.giera.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.marcin.giera.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
